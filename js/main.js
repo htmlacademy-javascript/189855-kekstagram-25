226d543a6b2ec4af5c656f3df1af013ba9075309
Aleksandr Chernobrov <alesandro83@mail.ru>
2022-04-06 01:24:38 +0300

    Надо подкачаться. Проблемы с промисами

--- js/main.js
+++ js/main.js
@@ -4,7 +4,6 @@ import {createLoader} from './api.js';
 import { errorMessage } from './messages.js';
 
 const photoLoader = createLoader(renderMini, errorMessage);
-//setUserFormSubmit(closeEditFormModal);
 
 photoLoader();
 activateFileLoader();
